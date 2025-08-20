87f60e263015dc8f8603c21cfe34581c27777cbc
Hari Patel <patelhariv18@gmail.com>
2025-08-20 22:04:36 +0530

    Initial project setup with Task model and basic API endpoints

---
+++ task_manager/urls.py
@@ -0,0 +1,9 @@
+# task_manager/urls.py
+
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/', include('tasks.urls')),
+]
